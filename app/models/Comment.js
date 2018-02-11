2f33336002b8b7ff891b33623ca44432d17ae05e
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-02-11 16:31:42 +0300

    Comment router and mondel refactor

--- app/models/Comment.js
+++ app/models/Comment.js
@@ -5,7 +5,14 @@ var	CommentSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
 	post: { type: Schema.Types.ObjectId, required: true, ref: 'Post' },
 	text: String,
-	isDeleted: Boolean
+	isDeleted: { type: Boolean, default: false }
 });
+
+CommentSchema.method.update = async (data) => {
+	for (field in data) {
+		this[field] = data[field];
+	}
+	await this.save();
+}
 
 mongoose.model('Comment', CommentSchema);
--- app/routes/comment.js
+++ app/routes/comment.js
@@ -2,63 +2,74 @@ const router	= require('express').Router();
 const passport	= require('passport');
 const mongoose	= require('mongoose');
 const auth		= require('../middleware/authentication');
+const ObjectId	= require('mongoose').Types.ObjectId;
+const util		= require('../util/util');
 const Comment	= mongoose.model('Comment');
+const Post		= mongoose.model('Post');
 
-router.post('/add', auth.isAuthenticated, (req, res, next) => {
-	var postId = req.body.post;
+router.post('/add', auth.isAuthenticated, async (req, res, next) => {
 	var comment = new Comment({
 		user: req.user._id,
-		post: postId,
+		post: req.body.post,
 		text: req.body.text
 	});
-	comment.save((err) => {
-		err ? next(err) : res.send('Comment created');
-	});
+	try {
+		if (!ObjectId.isValid(comment.post)) throw new Error('My error');
+		var post = await Post.findById(comment.post);
+		post ? await comment.save() : _throw(new Error('My Error'));
+	} catch (e) {
+		return next(e);
+	}
+	res.send('Comment created');
 });
 
-router.post('/edit', auth.isAdmin, (req, res, next) => {
+router.post('/edit', auth.isAdmin, async (req, res, next) => {
 	var commentId = req.body.comment;
-	Comment.findById(commentId, (err, comment) => {
-		if (err) return next(err);
-
+	try {
+		if (!ObjectId.isValid(commentId)) throw new Error('My error');
+		var comment = await Comment.findById(commentId);
 		if (comment) {
-			comment.text = req.body.text;
-			comment.save((err) => {
-				return err ? next(err) : res.send('Comment has been updeate');
-			});
-		} else {
-			res.status(404).end()
-		}
-	});
-});
-
-router.post('/delete', auth.isAdmin, (req, res, next) => {
-	var commentId = req.body.comment;
-	Comment.findById(commentId, (err, comment) => {
-		if (err) return next(err);
-
-		if (comment) {
-			comment.isDeleted = true;
-			comment.save((err) => {
-				return err ? next(err) : res.send('Comment has been deleted');
-			});
+			await comment.update({ text: req.body.text });
 		} else {
 			res.status(404).end();
 		}
-	});
+	} catch (e) {
+		return next(e);
+	}
+	res.send('Comment has been updated');
 });
 
-router.get('/list', (req, res, next) => {
+router.post('/delete', auth.isAdmin, async (req, res, next) => {
+	var commentId = req.body.comment;
+	try {
+		if (!ObjectId.isValid(commentId)) throw new Error('My error');
+		var comment = await Comment.findById(commentId);
+		if (comment) {
+			await comment.update({ isDeleted: true });
+		} else {
+			res.status(404).end();
+		}
+	} catch (e) {
+		return next(e);
+	}
+	res.send('Comment has been deleted');
+});
+
+router.get('/list', async (req, res, next) => {
 	var perPage = Number.parseInt(req.query.perpage) || 5;
 	var page = Number.parseInt(req.query.page) || 0;
 	var postId = req.query.post;
-	Comment.find({ 'post': postId })
-		.where('isDeleted').ne(true)
-		.limit(perPage)
-		.skip(perPage * page)
-		.exec((err, comments) => {
-			return err ? next(err) : res.json(comments);
-		});
+	try {
+		if (!ObjectId.isValid(postId)) throw new Error('My error');
+		var comments = await Comment.find({ 'post': postId })
+			.where('isDeleted').ne(true)
+			.limit(perPage)
+			.skip(perPage * page)
+			.exec();
+	} catch (e) {
+		return next(err);
+	}
+	res.json(comments);
 });
 
 module.exports = router;
