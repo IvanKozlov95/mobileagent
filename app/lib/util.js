1f9120f7f614aaa546a2d0d35698a76a3ec316b1
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-02-11 18:13:06 +0300

    Combined common code in utils

--- app/lib/util.js
+++ app/lib/util.js
@@ -1,3 +1,14 @@
-function _throw(error) {
+const ObjectId	= require('mongoose').Types.ObjectId;
+const Errors	= require('./error');
+
+module.exports._throw = function (error) {
 	throw error;
+}
+
+module.exports.checkObjectId = function (id) {
+	if (!ObjectId.isValid(id)) throw new Errors.BadRequest('Object id is not correct');
+}
+
+module.exports.checkText = function (text) {
+	if (text == '' || text == undefined) throw new Errors.BadRequest('Text cannot be empty');
 }
