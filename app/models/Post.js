93a4704830a59620cf915f3c5371adddb346219c
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-02-11 16:06:14 +0300

    Removed unused field for Post model

--- app/models/Post.js
+++ app/models/Post.js
@@ -5,8 +5,6 @@ var	PostSchema = new Schema({
 	title: { type: String, required: true },
 	text: { type: String, required: true },
 	creator: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-	likes: Number,
-	dislikes: Number
 });
 
 mongoose.model('Post', PostSchema);
