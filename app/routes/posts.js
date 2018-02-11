ff8dfa21e078f27c07800e98a8a2829ccfd57dde
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-02-11 16:06:00 +0300

    Refactor posts router(async/await) feature

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -2,29 +2,29 @@ const router	= require('express').Router();
 const passport	= require('passport');
 const mongoose	= require('mongoose');
 const auth		= require('../middleware/authentication');
+const ObjectId	= require('mongoose').Types.ObjectId;
 const Post		= mongoose.model('Post');
 const Like		= mongoose.model('Like');
 
-router.post('/create', auth.isAdmin, (req, res, next) => {
+router.post('/create', auth.isAdmin, async (req, res, next) => {
 	var post = new Post({
 		title: req.body.title,
 		text: req.body.text,
-		creator: req.user._id,
-		likes: 0,
-		dislikes: 0
+		creator: req.user._id
 	})
-	console.log(req.body);
-	console.log(req.user);
-	post.save((err) => {
-		err ? next(err) : res.send('Post created');
-	});
+	try {
+		await post.save();
+	} catch (e) {
+		next(err);
+	};
+	res.send('Post created');
 });
 
-router.post('/like', auth.isAuthenticated, (req, res, next) => {
+router.post('/like', auth.isAuthenticated, async (req, res, next) => {
 	var postId = req.body.post;
-	Like.findOne({ 'user': req.user._id, 'post': postId }, (err, like) => {
-		if (err) return next(err);
-
+	try {
+		if (!ObjectId.isValid(postId)) throw new Error('My error');
+		var like = await Like.findOne({ 'user': req.user._id, 'post': postId });
 		if (like) {
 			like.value = req.body.value;
 		} else {
@@ -34,52 +34,52 @@ router.post('/like', auth.isAuthenticated, (req, res, next) => {
 				value: req.body.value
 			});
 		}
-		like.save((err) => {
-			return err ? next(err) : res.send('Like has been updated');
-		});
-	})
+		await like.save();
+	} catch (e) {
+		return next(e);
+	}
+	res.send('like asdasdasd');
 });
 
-router.post('/edit', auth.isAdmin, (req, res, next) => {
+router.post('/edit', auth.isAdmin, async (req, res, next) => {
 	var postId = req.body.post;
-
-	Post.findById(postId, (err, post) => {
-		if (err) return next(err);
-
+	try {
+		if (!ObjectId.isValid(req.query.id)) throw new Error('My error');
+		var post = await Post.findById(req.query.id);
 		if (post) {
 			post.text = req.body.text;
-			post.save((err) => {
-				return err ? next(err) : res.send('Post has been updated');
-			});
+			await post.save();
 		} else {
 			res.status(404).end();
 		}
-	});
+	} catch (e) {
+		return next(e);
+	}
+	res.send('Post has been updated');
 });
 
-router.get('/list', (req, res, next) => {
+router.get('/list', async (req, res, next) => {
 	var perPage = Number.parseInt(req.query.perpage) || 5;
 	var page = Number.parseInt(req.query.page) || 0;
-	Post.find()
-		.limit(perPage)
-		.skip(perPage * page)
-		.exec((err, posts) => {
-			return err ? next(err) : res.json(posts);
-		});
+	try {
+		var posts = await Post.find()
+				.limit(perPage)
+				.skip(perPage * page)
+				.exec();
+	} catch (e) {
+		return next(err);
+	}
+	res.json(posts);
 });
 
-router.get('/info', (req, res, next) => {
-	var postId = req.query.id;
-
-	Post.findById(postId, (err, post) => {
-		if (err) return next(err);
-
-		if (post) {
-			res.json(post);
-		} else {
-			res.status(404).end();
-		}
-	});
+router.get('/info', async (req, res, next) => {
+	try {
+		if (!ObjectId.isValid(req.query.id)) throw new Error('My error');
+		var post = await Post.findById(req.query.id);
+	} catch (e) {
+		return next(e);
+	}
+	post ? res.json(post) : res.status(404).end();
 });
 
 module.exports = router;
